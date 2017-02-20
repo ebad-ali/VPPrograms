1748319fc27fd76e5e064789f9359a0371490bb8
ebadali12 <ebad.ali12@yahoo.com>
2017-02-20 18:38:29 +0500

    firstproject

---
+++ Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int age;
+
+            Console.Write("Your Age? : ");
+            age = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your Age is "+age);
+
+
+        }
+    }
+}
